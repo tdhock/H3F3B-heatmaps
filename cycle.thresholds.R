687dacecf6dbd0af69b2790e10cf5bf709ba1722
Toby Dylan Hocking <tdhock5@gmail.com>
2016-04-30 13:42:29 +0200

    plates plot

--- cycle.thresholds.R
+++ cycle.thresholds.R
@@ -4,22 +4,70 @@ well.pattern <- paste0(
   "(?<letter>[A-P])",
   "(?<number>[0-9]+)")
 
-csv.path.vec <- Sys.glob("cycle.thresholds/*.csv")
+txt.path.vec <- Sys.glob("cycle.thresholds/*/*.txt")
 cycle.thresholds.list <- list()
-for(csv.path.i in seq_along(csv.path.vec)){
-  csv.path <- csv.path.vec[[csv.path.i]]
-  csv.line.vec <- readLines(csv.path)
-  print(header.row <- grep("^Well", csv.line.vec))
+for(txt.path.i in seq_along(txt.path.vec)){
+  txt.path <- txt.path.vec[[txt.path.i]]
+  csv.line.vec <- readLines(txt.path)
+  print(header.rows <- grep("^Well", csv.line.vec))
+  data.line.vec <- grep("^[0-9]+\t", csv.line.vec, value=TRUE)
+  first.header.row <- header.rows[1]
+  header.data.line.vec <- c(csv.line.vec[first.header.row], data.line.vec)
+  cleaned.path <- sub("txt$", "cleaned.csv", txt.path)
+  writeLines(header.data.line.vec, cleaned.path)
   ct.tab <- fread(
-    csv.path, skip=header.row-1, header=TRUE,
+    cleaned.path, header=TRUE,
+    ##nrows=last.table.row-header.row,
     na.strings="Undetermined",
-    select=c("CT", "Well Position"))
-  well.mat <- str_match_named(ct.tab$Well, well.pattern, list(
+    select=c("Ct", "Sample Name"))
+  well.mat <- str_match_named(ct.tab[["Sample Name"]], well.pattern, list(
     number=as.integer))
-  cycle.thresholds.list[[csv.path]] <- data.table(
-    csv.path.i, csv.path, well.mat, ct.tab,
+  only.data <- data.table(
+    txt.path.i, txt.path, well.mat, ct.tab,
     number.fac=factor(well.mat[, "number"]))
+  data.dir <- dirname(txt.path)
+  row.csv <- Sys.glob(file.path(data.dir, "*_Row.csv"))
+  row.info <- fread(row.csv)
+  table(row.info[["Plate Row"]])
+  table(only.data[["letter"]])
+  setkey(row.info, `Plate Row`)
+  setkey(only.data, letter)
+  data.row.info <- only.data[row.info]
+  col.csv <- Sys.glob(file.path(data.dir, "*_Column.csv"))
+  col.info <- fread(col.csv)
+  table(data.row.info[["number"]])
+  table(col.info[["Column"]])
+  setkey(col.info, Column)
+  setkey(data.row.info, number)
+  data.row.col.info <- data.row.info[col.info]
+  rbind(
+    data=nrow(ct.tab),
+    rows=nrow(row.info),
+    data.rows=nrow(data.row.info),
+    cols=nrow(col.info),
+    data.rows.cols=nrow(data.row.col.info))
+  cycle.thresholds.list[[paste("regular", txt.path)]] <- data.row.col.info
+  irregular.csv <- Sys.glob(file.path(data.dir, "*_Irregular.csv"))
+  if(length(irregular.csv)){
+    irregular.info <- fread(irregular.csv)
+    setkey(irregular.info, `Plate Row`, Column)
+    setkey(only.data, letter, number)
+    cycle.thresholds.list[[paste("irregular", txt.path)]] <-
+      only.data[irregular.info]
+  }
 }
-cycle.thresholds <- do.call(rbind, cycle.thresholds.list)
+
+(names.list <- lapply(cycle.thresholds.list, names))
+name.counts <- table(unlist(names.list))
+stopifnot(all(name.counts==length(names.list)))
+name.counts[order(name.counts)]
+
+ordered.columns.list <- list()
+for(data.name in names(cycle.thresholds.list)){
+  one.dt <- cycle.thresholds.list[[data.name]]
+  ordered.columns.list[[data.name]] <-
+    one.dt[, names(name.counts), with=FALSE]
+}
+cycle.thresholds <- do.call(rbind, ordered.columns.list)
 
 save(cycle.thresholds, file="cycle.thresholds.RData")
--- figure-cycle-thresholds.R
+++ figure-cycle-thresholds.R
@@ -5,8 +5,10 @@ load("cycle.thresholds.RData")
 platesPlot <- ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
-  facet_grid(. ~ csv.path)+
-  geom_tile(aes(x=letter, y=number.fac, fill=CT),
+  facet_grid(. ~ txt.path, labeller=function(var,val){
+    gsub("/", "\n", val)
+  })+
+  geom_tile(aes(x=letter, y=number.fac, fill=Ct),
             data=cycle.thresholds)
 
 pdf("figure-cycle-thresholds.pdf")
